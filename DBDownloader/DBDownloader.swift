b49e14eaaa58682bb705d7ead09a2258852ec264
Jeffrey Sulton <notlus68@me.com>
2015-11-16 08:18:36 -0800

    * Rename "DBDownloader" scheme and target to "PlaneDownloader"
    * Fix some compiler warnings

--- DBDownloader/DBDownloader.swift
+++ DBDownloader/DBDownloader.swift
@@ -135,9 +135,8 @@ class DBDownloader {
                     
                     print("Found \(jsonResults.count) aircraft")
                     
-                    let newResults = jsonResults.map({ rawElement -> DBPediaLookupResult in
+                    let _ = jsonResults.map({ rawElement -> DBPediaLookupResult in
                         let elementDict = rawElement as! [String: AnyObject]
-                        let uri1 = elementDict["uri"]
                         return DBPediaLookupResult(resourceURI: elementDict["uri"] as! String,
                             label: elementDict["label"] as! String,
                             abstract: elementDict["abstract"] as! String,
@@ -162,7 +161,7 @@ class DBDownloader {
                                 
                                 do {
                                     if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as? [String: AnyObject],
-                                        let resourceData = jsonResult[uri] as? [String: AnyObject] {
+                                        let _ = jsonResult[uri] as? [String: AnyObject] {
                                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
                                             do {
                                                 try self.sharedContext.save()
@@ -189,7 +188,7 @@ class DBDownloader {
         print("Downloading data for: \(url.absoluteString)")
         
         let request = NSURLRequest(URL: url)
-        let task = sharedSession.dataTaskWithRequest(request) { (data, response, error) -> Void in
+        let _ = sharedSession.dataTaskWithRequest(request) { (data, response, error) -> Void in
             if let error = error {
                 print("Error downloading data")
                 completion(data: nil, error: error)
@@ -228,5 +227,4 @@ class DBDownloader {
         
         return NSURL(string: fullURL)!
     }
-
 }
