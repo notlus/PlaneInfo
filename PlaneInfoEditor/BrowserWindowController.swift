cf2d1e76b0ad92eaae134ee2752e079c160f399f
Jeffrey Sulton <notlus68@me.com>
2016-02-13 00:01:59 -0800

    Add `BrowserWindowController` containing a table view to browse the aircraft

---
+++ PlaneInfoEditor/BrowserWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  BrowserWindowController.swift
+//  PlaneInfo
+//
+//  Created by Jeffrey Sulton on 2/11/16.
+//  Copyright © 2016 notluS. All rights reserved.
+//
+
+import Cocoa
+
+class BrowserWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
