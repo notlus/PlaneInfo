f8a4b360037dc9ec3b36082df0a7a9cefb42656f
Jeffrey Sulton <notlus68@me.com>
2016-02-14 22:23:13 -0800

    * Add `CheckboxTableCellView.swift`
    * Create "Views" and "Window Controllers" groups

---
+++ PlaneInfoEditor/CheckboxTableCellView.swift
@@ -0,0 +1,19 @@
+//
+//  CheckboxTableCellView.swift
+//  PlaneInfo
+//
+//  Created by Jeffrey Sulton on 2/14/16.
+//  Copyright © 2016 notluS. All rights reserved.
+//
+
+import Cocoa
+
+class CheckboxTableCellView: NSTableCellView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
